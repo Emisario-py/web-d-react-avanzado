d55fcabdab37f8b4aa7451e56cad5579ee9333e6
Emilio <EmilianoR117@outlook.com>
2025-07-22 19:35:42 -0700

    envio por post express

--- express/src/app.js
+++ express/src/app.js
@@ -39,6 +39,16 @@ app.get('/api/peliculas/comedia/:pais', (req, res) => {
   res.send(resultados)
 })
 
+app.use(express.json())
+app.post('/api/peliculas', (req, res) => {
+  const nuevaPelicula = req.body
+  console.log(nuevaPelicula)
+  res.status(201).send({
+    mensaje: 'La pelicula se recibio con éxito',
+    datos: nuevaPelicula
+  })
+})
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`)
 })
