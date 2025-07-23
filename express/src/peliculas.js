e30340fe21d3a10fb8082b3b7c8eb5828e2008fd
Emilio <EmilianoR117@outlook.com>
2025-07-22 19:24:00 -0700

    rutas con express

---
+++ express/src/peliculas.js
@@ -0,0 +1,44 @@
+const infoPeliculas = {
+  accion: [
+    {
+      id: 1,
+      titulo: 'Mision imposible',
+      year: 2015,
+      pais: 'usa'
+    },
+    {
+      id: 2,
+      titulo: 'John Wick',
+      year: 2014,
+      pais: 'usa'
+    },
+    {
+      id: 3,
+      titulo: 'Mad Max: Furia en el camino',
+      year: 2015,
+      pais: 'australia'
+    }
+  ],
+  comedia: [
+    {
+      id: 1,
+      titulo: 'Superbad',
+      year: 2007,
+      pais: 'usa'
+    },
+    {
+      id: 2,
+      titulo: 'La gran apuesta',
+      year: 2015,
+      pais: 'usa'
+    },
+    {
+      id: 3,
+      titulo: 'Nosotros los Nobles',
+      year: 2013,
+      pais: 'usa'
+    }
+  ]
+}
+
+module.exports.infoPeliculas = infoPeliculas
